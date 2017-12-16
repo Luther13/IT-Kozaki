54670a2f42828a12fd884d543aab8fa19063325c
Luther13 <poryckim@gmail.com>
2017-12-16 18:56:11 +0100

    Changing database to PostgreSQL

--- Copernicus_AIESEC/AppOne/admin.py
+++ Copernicus_AIESEC/AppOne/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from AppOne.models import Local_Committee
 # Register your models here.
+
+admin.site.register(Local_Committee)
--- Copernicus_AIESEC/AppOne/models.py
+++ Copernicus_AIESEC/AppOne/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
 # Create your models here.
+
 class Local_Comittee(models.Model):
     lc_name = models.CharField(max_length=64, unique=True)
